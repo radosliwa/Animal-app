2ceee16743c758f86e6ae14e53051ea0092f1473
Radek <sliwa.radoslaw2@gmail.com>
2018-04-26 11:09:32 +0200

    engine almost done

--- app/temp/scripts/script.js
+++ app/temp/scripts/script.js
@@ -1,11 +1,12 @@
 
 
+
 let index;
 class Animalia{
 
   constructor(animalGallery){
+
     this.animalGallery = animalGallery;
-    this.animalArray = ['dog','racoon', 'cat'];
     this.startButton = $('#start');
     this.gameArea = $('.gamearea');
     this.cellToClick = $('.cell');
@@ -15,66 +16,68 @@ class Animalia{
     this.finalMessage = $('.finalMessage');
     this.startButton.fadeTo('slow', 1);
     this.events();
-
   }
 
   //-------------------------------------GAME BEGINS
+  events(){
+    this.startButton.click(()=> {this.startGame();});
+  }
+
   startGame (){
     this.startButton.hide();
     this.gameArea.fadeTo('fast', 1);
     this.showAnimal();
-
-
-    //this.getUserChoice();
+    this.getUserChoice();
   }
-  events(){
-    var self = this;
-    this.startButton.click(()=> {this.startGame();});
-    this.cellToClick.click(this.getUserChoice.bind(this));
 
-  }
   //------------------------------------- user engages
   getUserChoice (){
 
-    let userChoice = $(this.cellToClick).children().text();
-    console.log(userChoice);
-    if(this.animalGallery[index].includes(userChoice)){
-      $(this).addClass('cell__flipped');
-      this.cellToClick.off('click');
+    let userChoice;
+    let that = this;
+    this.cellToClick.on('click', function(e){
+      userChoice = e.target.innerText;
 
-      $(this.cellFront).addClass('front__non-hover');
-      $(this.message).addClass('animated bounceInLeft').show().delay(600).text('good job!').delay(500) //needed first delay to keep message still for a sec
-      .one('animationend', function(){
+      //let userChoice = $(this).children().text();
+      console.log(that.animalGallery);
 
-        $(this).addClass('animated bounceOutUp');
-        $('.animal').fadeOut(800);
+      if(that.animalGallery[index].includes(userChoice)){
+        $(this).addClass('cell__flipped').off('click');
 
-      }); //with on() fadeOut would fire even after else
+        $(that.cellFront).addClass('front__non-hover');
+        $(that.message).addClass('animated bounceInLeft').show().delay(600).text('good job!').delay(500) //needed first delay to keep message still for a sec
+        .one('animationend', function(){
 
-      setTimeout(()=>{
-        $(this).removeClass('cell__flipped');
-        $(this.cellFront).removeClass('front__non-hover').attr('style', "");
-        $(this.message).removeClass('animated bounceInLeft bounceOutUp').text("");
-        this.getUserNextChoice();
-      }, 2000);
+          $(this).addClass('animated bounceOutUp');
+          $('.animal').fadeOut(800);
 
-    } else{
+        }); //with on() fadeOut would fire even after else
 
-      $(this.cellFront).addClass('front__non-hover');
-      $(this.message).addClass('animated bounceInLeft').show().delay(1100).text('wrong, try again!').hide(600)
-      .one('animationend',function(){
-        $(this).removeClass('animated bounceInLeft');
-        $(this.cellFront).removeClass('front__non-hover');
+        setTimeout(()=>{
+          $(this).removeClass('cell__flipped');
+          $(that.cellFront).removeClass('front__non-hover').attr('style', "");
+          $(that.message).removeClass('animated bounceInLeft bounceOutUp').text("");
+          that.getUserNextChoice();
+        }, 2000);
 
-      });
-    }
+      } else {
+
+        $(this).addClass('front__non-hover');
+        $(that.message).addClass('animated bounceInLeft').show().delay(1100).text('wrong, try again!').hide(600)
+        .one('animationend',function(){
+          $(this).removeClass('animated bounceInLeft');
+          $(that.cellFront).removeClass('front__non-hover');
+
+        });
+      }
+    });
+
   }
 
 
   getUserNextChoice (){
 
     this.animalGallery.splice(index,1) // to avoid repeats
-
     this.showAnimal();
     //--------------------------------------------------------------GAME ENDS
 
@@ -90,15 +93,12 @@ class Animalia{
   }
 
   showAnimal(){
+
     this.getIndex();
-
     this.animalShowing.attr('src',this.animalGallery[index]).fadeIn();
-
-    //console.log(this.animalGallery);
   }
 
   getIndex(){
-
     index = Math.floor(Math.random()*this.animalGallery.length);
   }
 }
